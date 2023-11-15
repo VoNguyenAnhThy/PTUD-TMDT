80f2f4cb9b0665421a9a1390170fe0f3885e703c
NguyenMinhLuong <54277793+NguyenMinhLuong@users.noreply.github.com>
2023-11-15 16:57:08 +0700

    product-detail.js

--- js/product-detail.js
+++ js/product-detail.js
@@ -13,46 +13,25 @@ document.querySelector('#view-all-description').addEventListener('click', () => 
 
 let products = [
     {
-        name: 'JBL E55BT KEY BLACK',
-        image1: './images/JBL_E55BT_KEY_BLACK_6175_FS_x1-1605x1605px.png',
-        image2: './images/JBL_LIVE650BTNC_Product Image_Folded_Black.webp',
-        old_price: '400',
-        curr_price: '300'
+        name: 'Mẫu thiết kế áo bóng đá SMD002',
+        image1: './images/Bóng đá 3.jpg',
+        image2: './images/Bóng đá 5.jpg',
+        old_price: '400.000đ',
+        curr_price: '250.000đ'
     },
     {
-        name: 'JBL JR 310BT',
-        image1: './images/JBL_JR 310BT_Product Image_Hero_Skyblue.png',
-        image2: './images/JBL_JR 310BT_Product Image_Detail_Skyblue.png',
-        old_price: '400',
-        curr_price: '300'
+        name: 'Mẫu thiết kế áo bóng đá SMD001',
+        image1: './images/Bóng đá 4.jpg',
+        image2: './images/Bóng đá 1.jpg',
+        old_price: '400.000đ',
+        curr_price: '250.000đ'
     },
     {
-        name: 'JBL TUNE 750BTNC',
-        image1: './images/kisspng-beats-electronics-headphones-apple-beats-studio-red-headphones.png',
-        image2: './images/JBL_E55BT_KEY_RED_6063_FS_x1-1605x1605px.webp',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'JBL Horizon',
-        image1: './images/JBLHorizon_001_dvHAMaster.png',
-        image2: './images/JBLHorizon_BLK_002_dvHAMaster.webp',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'JBL Tune 220TWS',
-        image1: './images/JBL_TUNE220TWS_ProductImage_Pink_ChargingCaseOpen.png',
-        image2: './images/JBL_TUNE220TWS_ProductImage_Pink_Back.png',
-        old_price: '400',
-        curr_price: '300'
-    },
-    {
-        name: 'UA Project Rock',
-        image1: './images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_HERO.png',
-        image2: './images/190402_E1_FW19_EarbudsWCase_S13_0033-1_1605x1605_BACK.png',
-        old_price: '400',
-        curr_price: '300'
+        name: 'Mẫu thiết kế áo bóng đá SMD003',
+        image1: './images/Bóng đá 6.jpg',
+        image2: './images/Bóng đá 7.jpg',
+        old_price: '400.000đ',
+        curr_price: '250.000đ'
     },
 ]
 

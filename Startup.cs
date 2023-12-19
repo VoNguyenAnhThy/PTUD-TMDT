2f9edabb1f96be9ecac589169c3d0271657b603b
Anthie <148529418+VoNguyenAnhThy@users.noreply.github.com>
2023-12-19 22:14:42 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(S_me.Startup))]
+namespace S_me
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
